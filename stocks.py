423017935ec79c16ebe48763b539d655d396db1b
Kaushik <kaushik@getlocket.com>
2014-11-12 12:50:17 -0800

    more functions for operation.

--- stocks.py
+++ stocks.py
@@ -3,15 +3,24 @@ import glob
 import fnmatch
 import os
 import urllib2
+import csv
+
+		
+def refreshStocks():
+	print 'refreshing all the downloads'
+	downloadStocks(listStocks())
 
 def listStocks():
-	print 'List the stocks available for analysis:' 
+	listOfStocks = []
 	for file in os.listdir('./stocks/'):
 		if fnmatch.fnmatch(file, '*.csv'):
-			print file.split('.',1)[0]
+			fileName = file.split('.',1)[0]
+			print fileName
+			listOfStocks.append(fileName)
+	return listOfStocks
 
 def downloadFile(url, filename):
-	print 'downloading each file'
+	print 'downloading ' + filename
 	#"http://ichart.finance.yahoo.com/table.csv?s=AAPL&amp;d=1&amp;e=1&amp;f=2014&amp;g=d&amp;a=8&amp;b=7&amp;c=1984&amp;ignore=.csv")
 	# Retrieve the webpage as a string
 	response = urllib2.urlopen(url)
@@ -24,59 +33,172 @@ def downloadFile(url, filename):
 		f.write(line + "\n")
 	f.close()
 			
-''' http://real-chart.finance.yahoo.com/table.csv?s=AMZN&d=10&e=12&f=2014&g=d&a=4&b=16&c=1997&ignore=.csv ''' 
 def downloadStocks(args):
 	if not os.path.exists('./stocks/'):
 		os.makedirs('./stocks/')
-	print 'downloading all the files'
 	argNumber = len(args)
-	print argNumber
-	count=1
+	count=0
 	while (count < argNumber):
-		downloadFile('http://real-chart.finance.yahoo.com/table.csv?s='+args[1+count]+'&d=10&e=12&f=2014&g=d&a=4&b=16&c=1997&ignore=.csv', args[1+count]+'.csv')
+		downloadFile('http://real-chart.finance.yahoo.com/table.csv?s='+args[count]+'&d=10&e=12&f=2014&g=d&a=4&b=16&c=1997&ignore=.csv', args[count]+'.csv')
 		count = count + 1
-		
-def refreshStocks():
-	print 'refreshing all the downloads'
 	
-def maxDayIncrease():
-	print 'max_day_increase'
+def mod(val):
+	try:
+	 if val<0:
+		return val*(-1)
+	 else:
+		return val
+	except:
+		return val
 	
-def maxDayDecrease():
-	print 'max_day_decrease'
+def maxDayIncrease(args):
+    #print 'max_day_increase'
+    fileName = './stocks/' +args[0] + '.csv'
+    maxDifference=0.0
+    openPrice=''
+    closePrice=''
+    bestDate=''
+    with open(fileName, 'rb') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			try:
+				if((float(row[4]) - float(row[1])) > maxDifference):
+					maxDifference = float(row[4]) - float(row[1])
+					openPrice = row[1]
+					closePrice = row[4]
+					bestDate =row[0]
+				#print 'open :' + openPrice + ', close:' + closePrice + ', maximum:' + str(maxDifference)
+			except ValueError:
+				pass
+				#print 'ValueError'
+			except:
+				pass
+				#print 'Exception ' + str(row)
+	#FOO: 2013-10-23 open = 22.00 close = 100.00 diff = 78.00
+    print args[0] + ':' + bestDate + ' open = ' + openPrice + ' close = ' + closePrice + ' diff = ' + str(mod(maxDifference))
 
-def maxDayPercentIncrease():
-	print 'max_day_percent_increase' 
+def maxDayDecrease(args):
+	#print 'max_day_decrease'
+	fileName = './stocks/' +args[0] + '.csv'
+	maxDifference=0.0
+	openPrice=''
+	closePrice=''
+	bestDate=''
+	with open(fileName, 'rb') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			try:
+				if(float(row[4]) - float(row[1]) < maxDifference):
+					maxDifference = float(row[4]) - float(row[1])
+					openPrice = row[1]
+					closePrice = row[4]
+					bestDate =row[0]
+				#print 'open :' + openPrice + ', close:' + closePrice + ', maximum:' + str(maxDifference)
+			except ValueError:
+				pass
+				#print 'ValueError'
+			except:
+				pass
+				#print 'Exception ' + str(row)
+	#FOO: 2013-10-23 open = 22.00 close = 100.00 diff = 78.00
+	print args[0] +':'+bestDate+ ' open = ' + openPrice + ' close = ' + closePrice + ' diff =' + str(mod(maxDifference))
 
-def maxDayPercentDecrease():
-	print 'max_day_percent_decrease'
+def maxDayPercentIncrease(args):
+	#print 'max_day_percent_increase' 
+	#print 'max_day_decrease'
+	fileName = './stocks/' +args[0] + '.csv'
+	maxDifference=0.0
+	openPrice=''
+	closePrice=''
+	bestDate=''
+	with open(fileName, 'rb') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			try:
+				if((((float(row[4]) - float(row[1]))/float(row[1]))*100) > maxDifference):
+					maxDifference = (((float(row[4]) - float(row[1]))/float(row[1]))*100)
+					openPrice = row[1]
+					closePrice = row[4]
+					bestDate =row[0]
+				#print 'open :' + openPrice + ', close:' + closePrice + ', maximum:' + str(maxDifference)
+			except ValueError:
+				pass
+				#print 'ValueError'
+			except:
+				pass
+				#print 'Exception ' + str(row)
+	#FOO: 2013-10-23 open = 22.00 close = 100.00 diff = 78.00
+	print args[0] +':'+bestDate +' open = ' + openPrice + ' close = ' + closePrice + ' diff =' + str(mod(maxDifference)) + '%'
+
+def maxDayPercentDecrease(args):
+	#print 'max_day_percent_decrease'
+	fileName = './stocks/' +args[0] + '.csv'
+	maxDifference=0.0
+	openPrice=''
+	closePrice=''
+	bestDate=''
+	with open(fileName, 'rb') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			try:
+				if(((float(row[4]) - float(row[1]))/float(row[1]))*100 < maxDifference):
+					maxDifference = ((float(row[4]) - float(row[1]))/float(row[1]))*100
+					openPrice = row[1]
+					closePrice = row[4]
+					bestDate =row[0]
+				#print 'open :' + openPrice + ', close:' + closePrice + ', maximum:' + str(maxDifference)
+			except ValueError:
+				pass
+				#print 'ValueError'
+			except:
+				pass
+				#print 'Exception ' + str(row)
+	#FOO: 2013-10-23 open = 22.00 close = 100.00 diff = 78.00
+	print args[0] +':'+bestDate +' open = ' + openPrice + ' close = ' + closePrice + ' diff =' + str(mod(maxDifference)) + '%'
 	
-def whatIf():
+def whatIf(args):
 	print 'what_if'
 
 def top3():
 	print 'top3'
+	allStocksAvailable = listStocks()
+	for eachStock in allStocksAvailable:
+		print eachStock
 
 def main(args):
 	command = args[1]
+	# removing unnecessary params
+	args.pop(0)
+	args.pop(0)
 	if command == 'list':
+		print 'List the stocks available for analysis:' 
 		listStocks()
 	elif command == 'download':
 		downloadStocks(args)
 	elif command == 'refresh':
 	    refreshStocks()
 	elif command == 'max_day_increase':
-		maxDayIncrease()
+		while len(args) > 0:
+			maxDayIncrease(args)
+			args.pop(0)
 	elif command == 'max_day_decrease':
-		maxDayDecrease()
+		while len(args) > 0:
+			maxDayDecrease(args)
+			args.pop(0)
 	elif command == 'max_day_percent_increase':
-		maxDayPercentIncrease()
+		while len(args) > 0:
+			maxDayPercentIncrease(args)
+			args.pop(0)		
 	elif command == 'max_day_percent_decrease':
-		maxDayPercentDecrease()
+		while len(args) > 0:
+			maxDayPercentDecrease(args)
+			args.pop(0)	
 	elif command == 'what_if':
-		whatIf()
+		whatIf(args)
 	elif command == 'top3':
 		top3()
+	else:
+		print 'enter valid args '
 
 if __name__=='__main__':
     sys.exit(main(sys.argv))
